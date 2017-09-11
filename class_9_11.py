e8d257900ef0f2589848dd1b205200c6c949a82f
Brian Will <bmwill1995@gmail.com>
2017-09-11 14:51:51 -0400

    adding class noted from 9/11

---
+++ class_9_11.py
@@ -0,0 +1,39 @@
+#super basic math 
+print(4+4) 
+#print(((16/2) + 12)) / (8 * 3.1))
+
+#powers and such
+print( 3 ** 2) 
+print(9 ** .5) 
+
+#intro to variables 
+y = 12 
+x = 3.17 
+print( x, y) 
+print( x + (2 * y)) 
+
+#Lists 
+a = [1, 2, 3, 4, 5] 
+
+b = [2, "how did my father die?", 49, "times"]
+
+print(a) 
+print(b) 
+
+#combining lists 
+ab = a + b 
+print(ab) 
+ba = b + a 
+print(ba) 
+
+print(len(a)) 
+print(len(b))
+print(len(ba)) 
+print(len(a + b))
+
+#adding to lists 
+a.append(6) 
+print(a)
+a = a + [7]
+print(a) 
+
